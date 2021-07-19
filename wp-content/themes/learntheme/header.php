193684ee3599515ae84f03d0ce677bd6a866c947
ElDanto <pavlovian.ch@gmail.com>
2021-07-19 21:18:35 +0300

    Custom nav menu

--- wp-content/themes/learntheme/header.php
+++ wp-content/themes/learntheme/header.php
@@ -22,18 +22,48 @@
                 </div>
             </div>
         <?php
-         wp_nav_menu( [
-            'theme_location'  => 'top-menu',
-            'container'       => 'nav',
-            'container_class' => 'header-nav',
-            'container_id'    => null,
-            'menu_class'      => 'menu',
-            'menu_id'         => null,
-        ])?>
+        //  wp_nav_menu( [
+        //     'theme_location'  => 'top-menu',
+        //     'container'       => 'nav',
+        //     'container_class' => 'header-nav',
+        //     'container_id'    => null,
+        //     'menu_class'      => 'menu',
+        //     'menu_id'         => null,
+        // ])
+        ?>
         <?php 
-        ///$custom_menu = custom_get_menu('Главное меню');
+        $custom_menu = custom_get_menu('Главное меню');
         // var_dump($custom_menu);
 
         ?>
+        <nav class="header-nav">
+            <?php foreach($custom_menu as $key_menu => $item_menu):?>
+                <?php
+                // var_dump($item_menu);
+                    $title = !empty($item_menu->title) ? $item_menu->title : '';
+                    $url = !empty($item_menu->url) ? $item_menu->url : '';
+                    // echo $title;
+                ?>
+                
+                <?php if($item_menu->wpse_children):?>
+                    <span class="item-parent">
+                        <a href="<?php echo $url; ?>" class="header-nav-item <?php if ($item_menu->object_id == get_the_id()) {?>current-menu-item <?php };?>" > <?php echo $title; ?> </a>
+                        <div class="item-children">
+                            <?php foreach($item_menu->wpse_children as $key_children => $item_children):?>
+                                <?php
+                                    $title = !empty($item_children->title) ? $item_children->title : '';
+                                    $url = !empty($item_children->url) ? $item_children->url : '';
+                                    // echo $title;
+                                ?>
+                                <a href="<?php echo $url; ?>" class="header-nav-child <?php if ($item_children->object_id == get_the_id()) {?>current-menu-item <?php };?>" > <?php echo $title; ?> </a>
+                            <?php endforeach;?>
+                        </div>
+                    </span>
+                <?php else:?>
+                    <a href="<?php echo $url; ?>" class="header-nav-item <?php if ($item_menu->object_id == get_the_id()) {?>current-menu-item <?php };?>" > <?php echo $title; ?> </a>
+                <?php endif;?>
+            <?php endforeach; ?>
+        </nav>
 
         </header>
+        
